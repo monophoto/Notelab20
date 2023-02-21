e78cd24bd4f4de78639fae4aa513df50be34ef49
monophoto <jetsadawut1013@gmail.com>
2023-02-21 17:53:02 +0700

    cp

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -5,8 +5,14 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect R1, Rect R2){
+	double l1, l2;
+	l1 = min(R1.x+R1.w, R2.x+R2.w) - max(R1.x, R2.x);
+	l2 = min(R1.y,R2.y) - max(R1.y-R1.h,R2.y-R2.h);
+	if(l1 <= 0 || l2 <=0 ){
+	    return 0;
+	}else{
+	    return l1*l2;
+	}
+	
 }
